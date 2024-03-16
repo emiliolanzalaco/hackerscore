b11c06c177d38962daadab1128744d1af2e7ccab
emiliolanzalaco <emiliolanzalaco@hotmail.com>
2024-03-16 19:06:44 +0000

    refactor: useSmartAccountClient hook

--- packages/app/src/hooks/useSmartAccountClient.tsx
+++ packages/app/src/hooks/useSmartAccountClient.tsx
@@ -13,36 +13,36 @@ export function useSmartAccountClient() {
   const publicClient = usePublicClient()
   const [smartAccountClient, setSmartAccountClient] = useState<ReturnType<typeof createSmartAccountClient> | null>(null)
 
+  async function setUp() {
+    if (!walletClient?.account) return
+    const signer = walletClientToSmartAccountSigner(walletClient!)
+
+    const simpleSmartAccountClient = await signerToSafeSmartAccount(publicClient!, {
+      entryPoint: ENTRYPOINT_ADDRESS_V06,
+      signer,
+      safeVersion: '1.4.1',
+    })
+
+    const cloudPaymaster = createPimlicoPaymasterClient({
+      chain: baseSepolia,
+      transport: http(RPC_URL),
+      entryPoint: ENTRYPOINT_ADDRESS_V06,
+    })
+
+    const smartAccountClient = createSmartAccountClient({
+      account: simpleSmartAccountClient,
+      entryPoint: ENTRYPOINT_ADDRESS_V06,
+      chain: baseSepolia,
+      bundlerTransport: http(RPC_URL),
+      middleware: {
+        sponsorUserOperation: cloudPaymaster.sponsorUserOperation, // if using a paymaster
+      }
+    })
+
+    setSmartAccountClient(smartAccountClient as ReturnType<typeof createSmartAccountClient>)
+  }
+
   useEffect(() => {
-    async function setUp() {
-      if (!walletClient?.account) return
-      const signer = walletClientToSmartAccountSigner(walletClient!)
-
-      const simpleSmartAccountClient = await signerToSafeSmartAccount(publicClient!, {
-        entryPoint: ENTRYPOINT_ADDRESS_V06,
-        signer,
-        safeVersion: '1.4.1',
-      })
-
-      const cloudPaymaster = createPimlicoPaymasterClient({
-        chain: baseSepolia,
-        transport: http(RPC_URL),
-        entryPoint: ENTRYPOINT_ADDRESS_V06,
-      })
-
-      const smartAccountClient = createSmartAccountClient({
-        account: simpleSmartAccountClient,
-        entryPoint: ENTRYPOINT_ADDRESS_V06,
-        chain: baseSepolia,
-        bundlerTransport: http(RPC_URL),
-        middleware: {
-          sponsorUserOperation: cloudPaymaster.sponsorUserOperation, // if using a paymaster
-        }
-      })
-
-      setSmartAccountClient(smartAccountClient as ReturnType<typeof createSmartAccountClient>)
-    }
-
     if (account.address) {
       setUp()
     }
