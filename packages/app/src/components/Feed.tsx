67b4295bae0184e1b4df2b2b1d0dc8cb9038dff2
emiliolanzalaco <emiliolanzalaco@hotmail.com>
2024-03-17 08:39:13 +0000

    feat: finish feed

--- packages/app/src/components/Feed.tsx
+++ packages/app/src/components/Feed.tsx
@@ -1,6 +1,50 @@
 'use client'
 import '../styles/feed.css'
 import { gql, useQuery } from '@apollo/client'
+type Attestation = {
+  __typename: string
+  id: string
+  decodedDataJson: string
+  recipient: string
+  attester: string
+  timeCreated: number
+  txid: string
+  schemaId: string
+}
+
+const data = [
+  {
+    name: 'score',
+    type: 'uint256',
+    signature: 'uint256 score',
+    value: { name: 'score', type: 'uint256', value: { type: 'BigNumber', hex: '0x0a' } },
+  },
+  {
+    name: 'description',
+    type: 'string',
+    signature: 'string description',
+    value: {
+      name: 'description',
+      type: 'string',
+      value: 'White hat hacker found and patched an exploit worth $100 million.',
+    },
+  },
+]
+
+function extractScore(dataArray: string) {
+  const newDataArray: any[] = JSON.parse(dataArray)
+  const scoreObj = newDataArray.find((item) => item.name === 'score')
+  return Number(scoreObj.value.value.hex) // Assuming the score will always be a BigNumber object
+}
+
+function extractDescription(dataArray: string) {
+  const newDataArray: any[] = JSON.parse(dataArray)
+  const descriptionObj = newDataArray.find((item) => item.name === 'description')
+  if (!descriptionObj || !descriptionObj.value) {
+    return null // or a default description if not found
+  }
+  return descriptionObj.value.value // Directly returning the description string
+}
 
 const ATTESTATIONS_QUERY = gql`
   query Attestations($where: AttestationWhereInput, $orderBy: [AttestationOrderByWithRelationInput!]) {
@@ -16,7 +60,7 @@ const ATTESTATIONS_QUERY = gql`
   }
 `
 export function Feed() {
-  const {data} = useQuery(ATTESTATIONS_QUERY, {
+  const { data } = useQuery(ATTESTATIONS_QUERY, {
     variables: {
       where: {
         schemaId: {
@@ -27,33 +71,41 @@ export function Feed() {
         timeCreated: 'desc',
       },
     },
-  }) 
-  console.log(data)
-  return (  
+  }) as { data: { attestations: Attestation[] } }
+
+  if (!data) return <div>Loading...</div>
+  if (data.attestations.length === 0) return <div>No attestations found</div>
+
+  console.log(data.attestations[0].decodedDataJson)
+  return (
     <div className='container'>
       <div className='feed-container'>
-        <div className='panel panel-default'>
-          <div className='panel-heading'>
-            <h3 className='panel-title'>March 16, 2024, 10:00 AM - @user1</h3>
+        {data.attestations.map((attestation) => (
+          <div className='panel panel-default'>
+            <div className='panel-heading'>
+              <h3 className='panel-title'>
+                {new Date(attestation.timeCreated * 1000).toDateString()} - {attestation.attester}
+              </h3>
+            </div>
+            <div className='panel-body'>
+              <p>
+                <strong>About:</strong> {attestation.recipient}
+              </p>
+              <p>
+                <strong>X Factor:</strong> {extractScore(attestation.decodedDataJson)}
+              </p>
+              <p>
+                <strong>Description:</strong> {extractDescription(attestation.decodedDataJson)}
+              </p>
+            </div>
+            <div className='panel-footer'>
+              <strong>Transaction:</strong>{' '}
+              <a href={`https://sepolia.basescan.org/tx/${attestation.txid}`} className='post-link'>
+                View Transaction
+              </a>
+            </div>
           </div>
-          <div className='panel-body'>
-            <p>
-              <strong>About:</strong> @user2
-            </p>
-            <p>
-              <strong>X Factor:</strong> 95
-            </p>
-            <p>
-              <strong>Description:</strong> This is an example of a post in the feed. It contains a short description.
-            </p>
-          </div>
-          <div className='panel-footer'>
-            <strong>Transaction:</strong>{' '}
-            <a href='#' className='post-link'>
-              View Transaction
-            </a>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   )
